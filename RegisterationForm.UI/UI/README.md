e8cb94724cf9a3818e822fc67b2e0556b2d774e8
mmk90 <mmk90@yahoo.com>
2022-09-08 14:36:31 +0430

    commit

--- RegisterationForm.UI/UI/README.md
+++ RegisterationForm.UI/UI/README.md
@@ -1,6 +1,6 @@
 # UI
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.10.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.2.
 
 ## Development server
 
--- RegisterationForm.UI/UI/karma.conf.js
+++ RegisterationForm.UI/UI/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/UI'),
+      dir: require('path').join(__dirname, './coverage/ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
